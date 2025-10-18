dbd94cdd5d18a1d4d1b19b97c955736088207f75
João Gabriel Winck Mendes <joaogwmendes@gmail.com>
2025-10-18 00:43:31 +0000

    Implementa funcionalidade de login e logout, e adiciona componente TodoList com integração à API para listar tarefas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import React, { useState } from 'react';
+import { TodoList } from './components/TodoList'
+
 function App() {
+  const[isLogged, setIsLogged] = useState(false);
+  {/*isLogged é o elemento 0*/}
+  {/*setIsLoggedfunção que permite trocar o valor do isLogged. Precisa da função pq o isLogged é uma const*/}
+
+  const doLogin = () => setIsLogged(true);
+  const doLogout = () =>setIsLogged(false);
+
   return (
-    <h1 class="text-3xl font-bold underline text-red-700">
-      Hello world!
-    </h1>
-  )
+    <>
+    
+    {isLogged ? <div>Seja bem vindo, Fulando de tal! <a href="#" onClick={doLogout}>Clique aqui para deslogar</a> <TodoList /> </div> : <div>Faça seu login, <a href="#" onClick={doLogin}>Clique aqui!</a></div>} 
+    {/* Operador ternário para fazer o if */} 
+    
+    </>
+  
+   )
 }
 
 export default App
